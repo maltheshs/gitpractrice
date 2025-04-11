6893565ab7bb1f9c92fa178cfe8b357f8c7a0d62
maltheshs <maltheshs1998@gmail.com>
2025-04-11 23:15:07 +0530

    added implicit wait

--- src/test/java/com/practicegit/Test1.java
+++ src/test/java/com/practicegit/Test1.java
@@ -1,5 +1,7 @@
 package com.practicegit;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +12,8 @@ public static void main(String[] args) {
 	
 	driver.close();
 	driver.quit();
+	
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	
 }
 }
